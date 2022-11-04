9859cd5161b97bcb44caa719b825c5207101289c
esetiawan <esther.irawati@gmail.com>
2022-11-04 10:08:47 +0700

    bugfix

--- lib/utils/notification_helper.dart
+++ lib/utils/notification_helper.dart
@@ -7,6 +7,7 @@ import '../common/navigation.dart';
 import '../data/model/restaurant.dart';
 
 final selectNotificationSubject = BehaviorSubject<String>();
+
 class NotificationHelper {
   static NotificationHelper? _instance;
   NotificationHelper._internal() {
@@ -16,37 +17,45 @@ class NotificationHelper {
   Future<void> initNotifications(
       FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin) async {
     var initializeSettings = const AndroidInitializationSettings('app_icon');
-    await flutterLocalNotificationsPlugin.initialize(InitializationSettings(android:initializeSettings),
-        onDidReceiveBackgroundNotificationResponse: (NotificationResponse details) async {
-          final payload = details.payload;
-          if (payload!=null) {
-            print("notification payload:" + payload);
-          }
-          selectNotificationSubject.add(payload ?? 'empty payload');
-        });
+    await flutterLocalNotificationsPlugin.initialize(
+        InitializationSettings(android: initializeSettings),
+        onDidReceiveBackgroundNotificationResponse:
+            onDidReceiveBackgroundNotificationResponse1);
   }
+
+  static void onDidReceiveBackgroundNotificationResponse1(
+      NotificationResponse details) {
+    final payload = details.payload;
+    if (payload != null) {
+      print("notification payload:" + payload);
+    }
+    selectNotificationSubject.add(payload ?? 'empty payload');
+  }
+
   Future<void> showNotification(
       FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin,
       Restaurant resto) async {
     var channelId = "1";
-    var channelName= "channel_01";
+    var channelName = "channel_01";
     var channelDescription = "Resto Info Channel";
-    var androidPlatformChannelSpecifics = AndroidNotificationDetails
-      (channelId, channelName, channelDescription: channelDescription,
-        importance: Importance.max, priority: Priority.high,ticker:'ticker',
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+        channelId, channelName,
+        channelDescription: channelDescription,
+        importance: Importance.max,
+        priority: Priority.high,
+        ticker: 'ticker',
         styleInformation: const DefaultStyleInformation(true, true));
-    var titleNotification  = "<b> Resto Update </b>";
+    var titleNotification = "<b> Resto Update </b>";
     var titleTodo = resto.name;
     await flutterLocalNotificationsPlugin.show(0, titleNotification, titleTodo,
-        NotificationDetails(android:androidPlatformChannelSpecifics), payload: jsonEncode(resto.toJson()));
+        NotificationDetails(android: androidPlatformChannelSpecifics),
+        payload: jsonEncode(resto.toJson()));
   }
-  void configureSelectNotificationSubject(String route)
-  {
-    selectNotificationSubject.stream.listen(
-            (String payload) async {
-          var data = Restaurant.fromJson(json.decode(payload));
-          Navigation.intentWithData(route, data);
-        }
-    );
+
+  void configureSelectNotificationSubject(String route) {
+    selectNotificationSubject.stream.listen((String payload) async {
+      var data = Restaurant.fromJson(json.decode(payload));
+      Navigation.intentWithData(route, data);
+    });
   }
 }
